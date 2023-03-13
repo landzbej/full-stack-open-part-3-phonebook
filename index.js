4577eff58e8b26c76b2434a1c91f6d8407d37256
judah <landzbej@gmail.com>
2023-03-13 18:04:20 +0800

    production build of frontend added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 
@@ -32,6 +33,8 @@ let persons = [
   }
 ];
 
+app.use(cors());
+app.use(express.static('build'));
 app.use(express.json());
 morgan.token('toke', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :toke'));
@@ -97,7 +100,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, (() => {
   console.log(`Server running on port ${PORT}`)
 }))
